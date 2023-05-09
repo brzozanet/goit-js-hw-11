05cccb1eb35a41c9732d103151ae6fddddcd83ea
Paweł Brzozowski <pawel@brzoza.net>
2023-05-09 13:50:09 +0200

    Add pagination feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,35 @@
 import axios from "axios";
 import Notiflix from "notiflix";
+import { createGalleryItem } from "./createGalleryItem.js";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { createGalleryItem } from "./createGalleryItem";
 
-const API_URL = "https://pixabay.com/api/";
 const API_KEY = "8543283-ac41910cbcd5ccb3a6a09e0db";
-const PHOTO_PER_PAGE = 40;
-
-let querySearch = "";
-let page = 1;
-
+const API_URL = "https://pixabay.com/api/?";
+const LIMIT = 40;
 const searchFormEl = document.querySelector("#search-form");
-const galleryEl = document.querySelector(".gallery");
-const loadMoreBtnEl = document.querySelector(".load-more");
+const galleryEl = document.querySelector("#gallery");
+const loadMoreEl = document.querySelector("#load-more");
 
-const searchParams = () =>
+let currentPage = 1;
+let totalPages = 0;
+let querySearch = "";
+
+const createSearchParams = () =>
   new URLSearchParams({
     key: API_KEY,
     q: querySearch,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-    per_page: PHOTO_PER_PAGE,
+    page: currentPage,
+    per_page: LIMIT,
   });
 
 const fetchPhotos = async () => {
   try {
-    const response = await axios.get(`${API_URL}?${searchParams()}`);
+    const response = await axios.get(API_URL + createSearchParams());
+    if (response.data.hits.length === 0) throw new Error();
     return response.data;
   } catch (error) {
     Notiflix.Notify.failure(
@@ -36,21 +38,37 @@ const fetchPhotos = async () => {
   }
 };
 
+const createFirstPhotos = async () => {
+  const data = await fetchPhotos();
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  totalPages = Math.ceil(data.totalHits / LIMIT);
+  data.hits.forEach(photo => createGalleryItem(galleryEl, loadMoreEl, photo));
+  new SimpleLightbox("#gallery a");
+};
+
+const showMorePhotos = async () => {
+  currentPage++;
+  try {
+    if (currentPage > totalPages) throw new Error(error);
+    const data = await fetchPhotos();
+    data.hits.forEach(photo => createGalleryItem(galleryEl, loadMoreEl, photo));
+    new SimpleLightbox("#gallery a");
+  } catch (error) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+};
+
 const createGallery = event => {
-    event.preventDefault();
-    galleryEl.innerHTML = "";
-    querySearch = event.currentTarget.elements.searchQuery.value.trim();
-    if (querySearch === "") {
-        return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    }
-    fetchPhotos().then(data => {
-        if (data.hits.length === 0) {
-            return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        }
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        galleryEl.insertAdjacentHTML("beforeend", data.hits.map(createGalleryItem).join(""));
-    });
-    loadMoreBtnEl.classList.remove("is-hidden");
+  event.preventDefault();
+  querySearch = event.currentTarget.elements.searchQuery.value;
+  currentPage = 1;
+  totalPages = 0;
+  galleryEl.innerHTML = "";
+  loadMoreEl.classList.add("hidden");
+  createFirstPhotos();
 };
 
 searchFormEl.addEventListener("submit", createGallery);
+loadMoreEl.addEventListener("click", () => showMorePhotos());
--- src/js/createGalleryItem.js
+++ src/js/createGalleryItem.js
@@ -1,22 +1,16 @@
-export const createGalleryItem = image => {
-  const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
-  return `
-        <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+export const createGalleryItem = (element, button, photo) => {
+  button.classList.remove("hidden");
+  const galleryItem = document.createElement("div");
+  galleryItem.innerHTML = `
+    <div class="photo-card">
+        <a href="${photo.largeImageURL}"><img src="${photo.webformatURL}" alt="${photo.tags}" title="${photo.tags} loading="lazy" /></a>
         <div class="info">
-            <p class="info-item">
-            <b>Likes</b>: ${likes}
-            </p>
-            <p class="info-item">
-            <b>Views</b>: ${views}
-            </p>
-            <p class="info-item">
-            <b>Comments</b>: ${comments}
-            </p>
-            <p class="info-item">
-            <b>Downloads</b>: ${downloads}
-            </p>
+            <p class="info-item"><b>Likes</b>: ${photo.likes}</p>
+            <p class="info-item"><b>Views</b>: ${photo.views}</p>
+            <p class="info-item"><b>Comments</b>: ${photo.comments}</p>
+            <p class="info-item"><b>Downloads</b>: ${photo.downloads}</p>
         </div>
-        </div>
+    </div>
     `;
+  element.append(galleryItem);
 };
